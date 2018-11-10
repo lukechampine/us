54500630ea13c70cad8ef189bfccdbeaa28e1629
Luke <luke.champine@gmail.com>
2018-11-10 01:26:21 -0500

    renterhost: Add import comment

--- renterhost/consts.go
+++ renterhost/consts.go
@@ -1,6 +1,6 @@
 // +build !dev
 
-package renterhost
+package renterhost // import "lukechampine.com/us/renterhost"
 
 // SectorSize is the size of one sector in bytes.
 const SectorSize = 1 << 22 // 4 MiB
--- renterhost/consts_dev.go
+++ renterhost/consts_dev.go
@@ -1,6 +1,6 @@
 // +build dev
 
-package renterhost
+package renterhost // import "lukechampine.com/us/renterhost"
 
 // SectorSize is the size of one sector in bytes.
 const SectorSize = 1 << 18 // 256 KiB
