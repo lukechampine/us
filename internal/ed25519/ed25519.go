5ae77113a93b8fb949677af70487da118f01df23
lukechampine <luke.champine@gmail.com>
2018-11-23 18:07:18 -0500

    ed25519: Add internal/ed25519
    This is a copy of golang.org/x/crypto/ed25519, plus the performance
    improvements from https://go-review.googlesource.com/c/crypto/+/71950
    
    I also stripped out a lot of the code we don't need, and replaced the
    Sign/Verify functions with SignHash/VerifyHash methods that satisfy
    the interfaces in renterhost. The payoff here is that now we have a
    single definition of ed25519 keys, instead of the renter/host/hostdb
    all implementing the same tiny wrapper around the native ed25519
    package.
    
    A side benefit is that, since hashes are constant-size, SignHash and
    VerifyHash incur one less allocation than Sign and Verify. :P

---
+++ internal/ed25519/ed25519.go
@@ -0,0 +1,154 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package ed25519 implements the Ed25519 signature algorithm. See
+// https://ed25519.cr.yp.to/.
+//
+// These functions are also compatible with the “Ed25519” function defined in
+// https://tools.ietf.org/html/draft-irtf-cfrg-eddsa-05.
+package ed25519
+
+// This code is a port of the public domain, “ref10” implementation of ed25519
+// from SUPERCOP.
+
+import (
+	"bytes"
+	"crypto/sha512"
+	"strconv"
+
+	"gitlab.com/NebulousLabs/Sia/crypto"
+	"lukechampine.com/us/internal/ed25519/internal/edwards25519"
+)
+
+const (
+	// PublicKeySize is the size, in bytes, of public keys as used in this package.
+	PublicKeySize = 32
+	// PrivateKeySize is the size, in bytes, of private keys as used in this package.
+	PrivateKeySize = 64
+	// SignatureSize is the size, in bytes, of signatures generated and verified by this package.
+	SignatureSize = 64
+	// SeedSize is the size, in bytes, of private key seeds. These are the private key representations used by RFC 8032.
+	SeedSize = 32
+)
+
+// PublicKey is the type of Ed25519 public keys.
+type PublicKey []byte
+
+// VerifyHash reports whether sig is a valid signature of hash by pub.
+func (pub PublicKey) VerifyHash(hash crypto.Hash, sig []byte) bool {
+	if l := len(pub); l != PublicKeySize {
+		panic("ed25519: bad public key length: " + strconv.Itoa(l))
+	}
+
+	if len(sig) != SignatureSize || sig[63]&224 != 0 {
+		return false
+	}
+
+	var A edwards25519.ExtendedGroupElement
+	var publicKeyBytes [32]byte
+	copy(publicKeyBytes[:], pub)
+	if !A.FromBytes(&publicKeyBytes) {
+		return false
+	}
+	edwards25519.FeNeg(&A.X, &A.X)
+	edwards25519.FeNeg(&A.T, &A.T)
+
+	buf := make([]byte, 96)
+	copy(buf[:32], sig[:32])
+	copy(buf[32:], pub)
+	copy(buf[64:], hash[:])
+	digest := sha512.Sum512(buf)
+
+	var hReduced [32]byte
+	edwards25519.ScReduce(&hReduced, &digest)
+
+	var R edwards25519.ProjectiveGroupElement
+	var b [32]byte
+	copy(b[:], sig[32:])
+	edwards25519.GeDoubleScalarMultVartime(&R, &hReduced, &A, &b)
+
+	var checkR [32]byte
+	R.ToBytes(&checkR)
+	return bytes.Equal(sig[:32], checkR[:])
+}
+
+// PrivateKey is an Ed25519 private key.
+type PrivateKey []byte
+
+// SignHash signs a hash with priv and returns a signature.
+func (priv PrivateKey) SignHash(hash crypto.Hash) []byte {
+	if l := len(priv); l != PrivateKeySize {
+		panic("ed25519: bad private key length: " + strconv.Itoa(l))
+	}
+
+	digest1 := sha512.Sum512(priv[:32])
+
+	var expandedSecretKey [32]byte
+	copy(expandedSecretKey[:], digest1[:32])
+	expandedSecretKey[0] &= 248
+	expandedSecretKey[31] &= 63
+	expandedSecretKey[31] |= 64
+
+	buf := make([]byte, 96)
+	copy(buf[:32], digest1[32:])
+	copy(buf[32:], hash[:])
+	messageDigest := sha512.Sum512(buf[:64])
+
+	var messageDigestReduced [32]byte
+	edwards25519.ScReduce(&messageDigestReduced, &messageDigest)
+	var R edwards25519.ExtendedGroupElement
+	edwards25519.GeScalarMultBase(&R, &messageDigestReduced)
+
+	var encodedR [32]byte
+	R.ToBytes(&encodedR)
+
+	copy(buf[:32], encodedR[:])
+	copy(buf[32:], priv[32:])
+	copy(buf[64:], hash[:])
+	hramDigest := sha512.Sum512(buf[:96])
+
+	var hramDigestReduced [32]byte
+	edwards25519.ScReduce(&hramDigestReduced, &hramDigest)
+
+	var s [32]byte
+	edwards25519.ScMulAdd(&s, &hramDigestReduced, &expandedSecretKey, &messageDigestReduced)
+
+	signature := make([]byte, SignatureSize)
+	copy(signature[:32], encodedR[:])
+	copy(signature[32:], s[:])
+	return signature
+}
+
+// PublicKey returns the PublicKey corresponding to priv.
+func (priv PrivateKey) PublicKey() PublicKey {
+	return PublicKey(priv[32:])
+}
+
+// NewKeyFromSeed calculates a private key from a seed. It will panic if
+// len(seed) is not SeedSize. This function is provided for interoperability
+// with RFC 8032. RFC 8032's private keys correspond to seeds in this
+// package.
+func NewKeyFromSeed(seed []byte) PrivateKey {
+	if l := len(seed); l != SeedSize {
+		panic("ed25519: bad seed length: " + strconv.Itoa(l))
+	}
+
+	digest := sha512.Sum512(seed)
+	digest[0] &= 248
+	digest[31] &= 127
+	digest[31] |= 64
+
+	var A edwards25519.ExtendedGroupElement
+	var hBytes [32]byte
+	copy(hBytes[:], digest[:])
+	edwards25519.GeScalarMultBase(&A, &hBytes)
+	var publicKeyBytes [32]byte
+	A.ToBytes(&publicKeyBytes)
+
+	privateKey := make([]byte, PrivateKeySize)
+	copy(privateKey, seed)
+	copy(privateKey[32:], publicKeyBytes[:])
+
+	return privateKey
+}
---
+++ internal/ed25519/ed25519_test.go
@@ -0,0 +1,92 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ed25519
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"testing"
+
+	"gitlab.com/NebulousLabs/Sia/crypto"
+	"lukechampine.com/us/internal/ed25519/internal/edwards25519"
+)
+
+func generateKey() (PublicKey, PrivateKey) {
+	seed := make([]byte, SeedSize)
+	rand.Read(seed)
+	priv := NewKeyFromSeed(seed)
+	return priv.PublicKey(), priv
+}
+
+func TestUnmarshalMarshal(t *testing.T) {
+	pub, _ := generateKey()
+
+	var A edwards25519.ExtendedGroupElement
+	var pubBytes [32]byte
+	copy(pubBytes[:], pub)
+	if !A.FromBytes(&pubBytes) {
+		t.Fatalf("ExtendedGroupElement.FromBytes failed")
+	}
+
+	var pub2 [32]byte
+	A.ToBytes(&pub2)
+
+	if pubBytes != pub2 {
+		t.Errorf("FromBytes(%v)->ToBytes does not round-trip, got %x\n", pubBytes, pub2)
+	}
+}
+
+func TestSignVerify(t *testing.T) {
+	public, private := generateKey()
+
+	hash := crypto.Hash{0}
+	sig := private.SignHash(hash)
+	if !public.VerifyHash(hash, sig) {
+		t.Errorf("valid signature rejected")
+	}
+
+	wrongHash := crypto.Hash{1}
+	if public.VerifyHash(wrongHash, sig) {
+		t.Errorf("signature of different message accepted")
+	}
+}
+
+func TestGolden(t *testing.T) {
+	privBytes, _ := hex.DecodeString("8ed7a797b9cea8a8370d419136bcdf683b759d2e3c6947f17e13e2485aa9d420b49f3a78b1c6a7fca8f3466f33bc0e929f01fba04306c2a7465f46c3759316d9")
+	msg, _ := hex.DecodeString("a750c232933dc14b1184d86d8b4ce72e16d69744ba69818b6ac33b1d823bb2c3")
+	sig, _ := hex.DecodeString("04266c033b91c1322ceb3446c901ffcf3cc40c4034e887c9597ca1893ba7330becbbd8b48142ef35c012c6ba51a66df9308cb6268ad6b1e4b03e70102495790b")
+
+	priv := PrivateKey(privBytes)
+	var hash crypto.Hash
+	copy(hash[:], msg)
+	if !bytes.Equal(sig, priv.SignHash(hash)) {
+		t.Error("bad signature")
+	} else if !priv.PublicKey().VerifyHash(hash, sig) {
+		t.Error("signature failed to verify")
+	}
+}
+
+func BenchmarkHashSigning(b *testing.B) {
+	b.ReportAllocs()
+	_, priv := generateKey()
+	hash := sha256.Sum256([]byte("Hello, world!"))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		priv.SignHash(hash)
+	}
+}
+
+func BenchmarkHashVerification(b *testing.B) {
+	b.ReportAllocs()
+	pub, priv := generateKey()
+	hash := sha256.Sum256([]byte("Hello, world!"))
+	signature := priv.SignHash(hash)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		pub.VerifyHash(hash, signature)
+	}
+}
