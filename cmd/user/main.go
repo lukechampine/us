8d25d814b020046e4ec3e4e9c6fb8b085c64641e
lukechampine <luke.champine@gmail.com>
2019-03-13 15:00:41 -0400

    user: Bump version to v0.3.0
    This will be the last version of user that supports the old renter-
    host protocol.

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -361,7 +361,7 @@ func main() {
 		}
 		fallthrough
 	case versionCmd:
-		log.Printf("user v0.2.0\nCommit:     %s\nRelease:    %s\nGo version: %s %s/%s\nBuild Date: %s\n",
+		log.Printf("user v0.3.0\nCommit:     %s\nRelease:    %s\nGo version: %s %s/%s\nBuild Date: %s\n",
 			githash, build.Release, runtime.Version(), runtime.GOOS, runtime.GOARCH, builddate)
 
 	case scanCmd:
