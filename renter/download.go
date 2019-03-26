8817884a2c09815d33928514aaa45c0c67050607
lukechampine <luke.champine@gmail.com>
2019-03-26 02:30:24 -0400

    renter: Add (ShardDownloader).CopySection

--- renter/download.go
+++ renter/download.go
@@ -2,6 +2,7 @@ package renter
 
 import (
 	"bytes"
+	"io"
 
 	"lukechampine.com/us/hostdb"
 	"lukechampine.com/us/merkle"
@@ -29,6 +30,97 @@ type ShardDownloader struct {
 	Slices     []SectorSlice
 	Key        KeySeed
 	buf        bytes.Buffer
+}
+
+type cryptWriter struct {
+	w      io.Writer
+	slices []SectorSlice
+	key    KeySeed
+	off    int64
+}
+
+func calcSections(slices []SectorSlice, offset, length int64) ([]renterhost.RPCReadRequestSection, error) {
+	if offset < 0 || length < 0 {
+		return nil, errors.New("offset and length must be positive")
+	}
+	// seek to offset
+	var n int64
+	for i := range slices {
+		size := int64(slices[i].NumSegments) * merkle.SegmentSize
+		if n+size > offset {
+			slices = slices[i:]
+			break
+		}
+		n += size
+	}
+	// construct first section
+	var sections []renterhost.RPCReadRequestSection
+	s := renterhost.RPCReadRequestSection{
+		MerkleRoot: slices[0].MerkleRoot,
+		Offset:     slices[0].SegmentIndex*merkle.SegmentSize + uint32(offset-n),
+		Length:     slices[0].NumSegments*merkle.SegmentSize - uint32(offset-n),
+	}
+	sections = append(sections, s)
+	length -= int64(s.Length)
+	slices = slices[1:]
+	// construct remaining sections
+	for len(slices) > 0 && length > 0 {
+		s := renterhost.RPCReadRequestSection{
+			MerkleRoot: slices[0].MerkleRoot,
+			Offset:     slices[0].SegmentIndex * merkle.SegmentSize,
+			Length:     slices[0].NumSegments * merkle.SegmentSize,
+		}
+		sections = append(sections, s)
+		length -= int64(s.Length)
+		slices = slices[1:]
+	}
+	if length > 0 {
+		return nil, errors.New("offset+length is out of bounds")
+	}
+	// trim final section
+	if length < 0 {
+		sections[len(sections)-1].Length -= uint32(-length)
+	}
+	return sections, nil
+}
+
+func calcSlices(slices []SectorSlice, off int64) ([]SectorSlice, int64) {
+	var n int64
+	for i := range slices {
+		size := int64(slices[i].NumSegments) * merkle.SegmentSize
+		if n+size > off {
+			return slices[i:], off - n
+		}
+		n += size
+	}
+	panic("unreachable")
+}
+
+func (cw *cryptWriter) Write(p []byte) (int, error) {
+	slices, rem := calcSlices(cw.slices, cw.off)
+	b := bytes.NewBuffer(p)
+	for i := 0; i < len(slices) && b.Len() > 0; i++ {
+		s := slices[i]
+		if i == 0 {
+			s.SegmentIndex += uint32(rem / merkle.SegmentSize)
+		}
+		xchachaNonce := append(s.Nonce[:], make([]byte, 4)...)
+		bb := b.Next(int(s.NumSegments) * merkle.SegmentSize)
+		cw.key.XORKeyStream(bb, xchachaNonce, uint64(s.SegmentIndex))
+	}
+	cw.off += int64(len(p))
+	return cw.w.Write(p)
+}
+
+// CopySection downloads the requested section of the Shard, decrypts it, and
+// writes it to w.
+func (d *ShardDownloader) CopySection(w io.Writer, offset, length int64) error {
+	sections, err := calcSections(d.Slices, offset, length)
+	if err != nil {
+		return err
+	}
+	cw := &cryptWriter{w, d.Slices, d.Key, offset}
+	return d.Downloader.Read(cw, sections)
 }
 
 // DownloadAndDecrypt downloads the SectorSlice associated with chunkIndex.
--- renter/download_test.go
+++ renter/download_test.go
@@ -1,11 +1,68 @@
 package renter
 
 import (
+	"bytes"
 	"io/ioutil"
 	"testing"
 
+	"gitlab.com/NebulousLabs/Sia/crypto"
+	"gitlab.com/NebulousLabs/fastrand"
+	"lukechampine.com/us/merkle"
 	"lukechampine.com/us/renterhost"
 )
+
+func TestCopySection(t *testing.T) {
+	// construct "encrypted" sectors
+	sectors := map[crypto.Hash][]byte{
+		crypto.Hash{0}: fastrand.Bytes(1024),
+		crypto.Hash{1}: fastrand.Bytes(1024),
+		crypto.Hash{2}: fastrand.Bytes(1024),
+	}
+	// construct shard from sector data
+	slices := []SectorSlice{
+		{MerkleRoot: crypto.Hash{0}, SegmentIndex: 0, NumSegments: 1, Nonce: [20]byte{0}},
+		{MerkleRoot: crypto.Hash{1}, SegmentIndex: 1, NumSegments: 2, Nonce: [20]byte{1}},
+		{MerkleRoot: crypto.Hash{1}, SegmentIndex: 4, NumSegments: 1, Nonce: [20]byte{1}},
+		{MerkleRoot: crypto.Hash{0}, SegmentIndex: 7, NumSegments: 6, Nonce: [20]byte{0}},
+		{MerkleRoot: crypto.Hash{2}, SegmentIndex: 0, NumSegments: 6, Nonce: [20]byte{2}},
+	}
+	var key KeySeed
+	var shard []byte
+	for _, s := range slices {
+		off, n := s.SegmentIndex*merkle.SegmentSize, int(s.NumSegments*merkle.SegmentSize)
+		shard = append(shard, sectors[s.MerkleRoot][off:][:n]...)
+		nonce := append(s.Nonce[:], make([]byte, 4)...)
+		key.XORKeyStream(shard[len(shard)-n:], nonce, uint64(s.SegmentIndex))
+	}
+
+	tests := []struct {
+		offset, length int64
+	}{
+		{512, 512},
+		{64, 64},
+		{0, 64},
+		{0, 1024},
+		{0, 128},
+		{0, 0},
+		{64, 0},
+	}
+	for _, test := range tests {
+		sections, err := calcSections(slices, test.offset, test.length)
+		if err != nil {
+			t.Fatal(err)
+		}
+		var buf bytes.Buffer
+		cw := &cryptWriter{&buf, slices, key, test.offset}
+		for _, s := range sections {
+			// need to copy because cryptWriter modifies its argument
+			data := append([]byte(nil), sectors[s.MerkleRoot][s.Offset:][:s.Length]...)
+			cw.Write(data)
+		}
+		if !bytes.Equal(buf.Bytes(), shard[test.offset:][:test.length]) {
+			t.Fatal("retrieved data does not match")
+		}
+	}
+}
 
 func BenchmarkIdealDownload(b *testing.B) {
 	rsc := NewRSCode(10, 40)
