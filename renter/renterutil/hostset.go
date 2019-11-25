40ff8d4a15e4b667157375445cb740cb1fd60744
lukechampine <luke.champine@gmail.com>
2019-11-25 14:21:03 -0500

    renterutil: Remove DownloadChunkShards

--- renter/renterutil/hostset.go
+++ renter/renterutil/hostset.go
@@ -1,7 +1,6 @@
 package renterutil
 
 import (
-	"strings"
 	"sync"
 
 	"github.com/pkg/errors"
@@ -107,102 +106,4 @@ func NewHostSet(hkr renter.HostKeyResolver, currentHeight types.BlockHeight) *Ho
 		currentHeight: currentHeight,
 		sessions:      make(map[hostdb.HostPublicKey]*lockedHost),
 	}
-}
-
-// DownloadChunkShards downloads the shards of chunkIndex from hosts in
-// parallel. shardLen is the length of the first non-nil shard.
-//
-// The shards returned by DownloadChunkShards are only valid until the next
-// call to Sector on the shard's corresponding proto.Downloader.
-func DownloadChunkShards(hosts []*renter.ShardDownloader, chunkIndex int64, minShards int, cancel <-chan struct{}) (shards [][]byte, shardLen int, err error) {
-	errNoHost := errors.New("no downloader for this host")
-	type result struct {
-		shardIndex int
-		shard      []byte
-		err        error
-	}
-	// spawn minShards goroutines that receive download requests from
-	// reqChan and send responses to resChan.
-	reqChan := make(chan int, minShards)
-	resChan := make(chan result, minShards)
-	var wg sync.WaitGroup
-	reqIndex := 0
-	for ; reqIndex < minShards; reqIndex++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for shardIndex := range reqChan {
-				res := result{shardIndex: shardIndex}
-				host := hosts[shardIndex]
-				if host == nil {
-					res.err = errNoHost
-				} else {
-					res.shard, res.err = host.DownloadAndDecrypt(chunkIndex)
-					res.err = errors.Wrap(res.err, host.HostKey().ShortKey())
-				}
-				resChan <- res
-			}
-		}()
-		// prepopulate reqChan with first minShards shards
-		reqChan <- reqIndex
-	}
-	// make sure all goroutines exit before returning
-	defer func() {
-		close(reqChan)
-		wg.Wait()
-	}()
-
-	// collect the results of each shard download, appending successful
-	// downloads to goodRes and failed downloads to badRes. If a download
-	// fails, send the next untried shard index. Break as soon as we have
-	// minShards successful downloads or if the number of failures makes it
-	// impossible to recover the chunk.
-	var goodRes, badRes []result
-	for len(goodRes) < minShards && len(badRes) <= len(hosts)-minShards {
-		select {
-		case <-cancel:
-			return nil, 0, ErrCanceled
-
-		case res := <-resChan:
-			if res.err == nil {
-				goodRes = append(goodRes, res)
-			} else {
-				badRes = append(badRes, res)
-				if reqIndex < len(hosts) {
-					reqChan <- reqIndex
-					reqIndex++
-				}
-			}
-		}
-	}
-	if len(goodRes) < minShards {
-		var errStrings []string
-		for _, r := range badRes {
-			errStrings = append(errStrings, r.err.Error())
-		}
-		return nil, 0, errors.New("too many hosts did not supply their shard:\n" + strings.Join(errStrings, "\n"))
-	}
-
-	shards = make([][]byte, len(hosts))
-	for _, r := range goodRes {
-		shards[r.shardIndex] = r.shard
-	}
-
-	// determine shardLen
-	for _, s := range shards {
-		if len(s) > 0 {
-			shardLen = len(s)
-			break
-		}
-	}
-
-	// allocate space for missing shards, in case the caller wants to
-	// reconstruct them
-	for i := range shards {
-		if len(shards[i]) == 0 {
-			shards[i] = make([]byte, 0, shardLen)
-		}
-	}
-
-	return shards, shardLen, nil
 }
